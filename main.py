5fdd9bfb2da159699a3e3d271b1b7460288e0ccb
Duz91 <110282202+Duz91@users.noreply.github.com>
2025-10-29 14:24:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -119,4 +119,4 @@ def main_loop():
 if __name__ == "__main__":
     threading.Thread(target=main_loop, daemon=True).start()
     threading.Thread(target=button_monitor, daemon=True).start()  # Starte den Button-Monitor-Thread
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
